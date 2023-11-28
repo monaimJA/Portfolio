bcf0a20cb16b9bd126f62a830c70371784e97048
Monaime ENNABBALI <monaim.ennabbali@usmba.ac.ma>
2023-11-29 00:49:02 +0100

    my portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ArchiveComponent } from './components/archive/archive.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: ':language?',      component: HomeComponent},
+  {path: ':language?/proyectos',   component: ArchiveComponent},
+
+  // {path: 'profile'        ,   component: ProfileComponent         , canActivate: [AuthGuard]},
+  // {path: 'users'          ,   component: UsersComponent           , canActivate: [AuthGuard]},
+  // {path: 'register-user'  ,   component: RegisterUserComponent    , canActivate: [AuthGuard]},
+
+  {path: '**', pathMatch: 'full', redirectTo: '/'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
-import { NgModule } from '@angular/core';
+import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { NgxGoogleAnalyticsModule } from 'ngx-google-analytics';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GeneralModule } from './components/general/general.module';
+import { HomeModule } from './components/home/home.module';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+export function HttpLoaderFactory(http: HttpClient){
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    GeneralModule,
+    BrowserAnimationsModule,
+    HomeModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    }),
+    NgbModule,
+    NgxGoogleAnalyticsModule.forRoot(environment.trackAnalyticID),
+    TranslateModule.forRoot({
+      loader:{
+        provide:TranslateLoader,
+        useFactory:HttpLoaderFactory,
+        deps:[HttpClient]
+      }
+    })
   ],
-  providers: [],
+  providers: [TranslateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
